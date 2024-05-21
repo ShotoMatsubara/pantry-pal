586c2d74621ceea579fa1dfe236baa83a7b0c22c
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-05-21 09:17:03 +0900

    build落ち対応

--- front/src/app/category/[category_name]/page.tsx
+++ front/src/app/category/[category_name]/page.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { Category } from '@/app/main/page';
 import React from 'react';
-
 import ClientSidePage from './ClientSidePage';
 
 type CategoryProps = {
@@ -10,9 +9,24 @@ type CategoryProps = {
   };
 };
 
-const CategoryPage = ({ params }: CategoryProps) => {
+const CategoryPage = async ({ params }: CategoryProps) => {
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_ROOT_URL;
   const categoryName = params.category_name;
-  return <ClientSidePage categoryName={categoryName} />;
+
+  try {
+    const result = await axios.get(`${backendUrl}/api/categories`);
+    const categories = result.data.categories;
+    const category = categories.find((cat: Category) => cat.category_name === categoryName);
+
+    if (!category) {
+      return <div>Category not found</div>;
+    }
+
+    return <ClientSidePage categoryName={category.category_name} />;
+  } catch (error) {
+    console.log(error);
+    return <div>Error occurred while fetching data</div>;
+  }
 };
 
 export default CategoryPage;
@@ -23,6 +37,7 @@ export const generateStaticParams = async () => {
     const result = await axios.get(`${backendUrl}/api/categories`);
     const categories = result.data.categories;
     console.log(categories);
+
     return categories.map((category: Category) => ({
       category_name: category.category_name,
     }));
